874b618a75b26f812c3ffcd5211a10c8c9b6b077
wxvirus <1418667580@qq.com>
2021-12-12 23:52:31 +0800

    3-7 封装请求参数和返回参数

--- src/main/java/com/wx/wiki/controller/EbookController.java
+++ src/main/java/com/wx/wiki/controller/EbookController.java
@@ -1,7 +1,8 @@
 package com.wx.wiki.controller;
 
-import com.wx.wiki.domain.Ebook;
+import com.wx.wiki.req.EbookReq;
 import com.wx.wiki.resp.CommonResp;
+import com.wx.wiki.resp.EbookResp;
 import com.wx.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +19,9 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp<List<Ebook>> list() {
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp<List<EbookResp>> list(EbookReq req) {
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
--- src/main/java/com/wx/wiki/service/EbookService.java
+++ src/main/java/com/wx/wiki/service/EbookService.java
@@ -3,9 +3,13 @@ package com.wx.wiki.service;
 import com.wx.wiki.domain.Ebook;
 import com.wx.wiki.domain.EbookExample;
 import com.wx.wiki.mapper.EbookMapper;
+import com.wx.wiki.req.EbookReq;
+import com.wx.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,7 +18,21 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list() {
-        return ebookMapper.selectByExample(new EbookExample());
+    public List<EbookResp> list(EbookReq req) {
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%" + req.getName() + "%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        List<EbookResp> respList = new ArrayList<>();
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            // 一个一个写，就比较麻烦
+//            ebookResp.setId(ebook.getId());
+            BeanUtils.copyProperties(ebook, ebookResp); // 从哪里拷贝到哪里 实现对象的复制
+            respList.add(ebookResp);
+        }
+
+        return respList;
     }
 }
