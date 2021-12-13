3cba804d7edecfa249dd3b85be0923459d6feda5
wxvirus <1418667580@qq.com>
2021-12-13 23:05:41 +0800

    4-5 集成ant design vue 加入按钮示例

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <a-button type="primary">Primary Button</a-button>
+    <a-button>Default Button</a-button>
+    <a-button type="dashed">Dashed Button</a-button>
+    <a-button type="text">Text Button</a-button>
+    <a-button type="link">Link Button</a-button>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
