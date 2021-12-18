65137d6ac5dc93b38f691d954c85c45295232a22
wxvirus <1418667580@qq.com>
2021-12-18 15:53:20 +0800

    5-9 增加AOP 打印接口耗时，请求参数和返回参数去掉过滤器、拦截器

--- src/main/java/com/wx/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/wx/wiki/config/SpringMvcConfig.java
@@ -1,23 +1,23 @@
-package com.wx.wiki.config;
-
-import com.wx.wiki.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LogInterceptor logInterceptor;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        // 注册一下拦截器
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/login");// 不是所有接口都需要进行拦截
-    }
-}
+//package com.wx.wiki.config;
+//
+//import com.wx.wiki.interceptor.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LogInterceptor logInterceptor;
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        // 注册一下拦截器
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns("/login");// 不是所有接口都需要进行拦截
+//    }
+//}
