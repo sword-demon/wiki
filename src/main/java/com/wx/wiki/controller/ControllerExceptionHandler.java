1bc0799bda42f5197e081f6bdfcd2727d042504f
wxvirus <1418667580@qq.com>
2021-12-20 22:34:43 +0800

    6-11 集成Validation， 为列表接口增加参数校验

---
+++ src/main/java/com/wx/wiki/controller/ControllerExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.wx.wiki.controller;
+
+import com.wx.wiki.resp.CommonResp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+/**
+ * 统一异常处理、数据预处理
+ */
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+
+    @ExceptionHandler(value = BindException.class)
+    @ResponseBody
+    public CommonResp validExceptionHandler(BindException e) {
+        CommonResp commonResp = new CommonResp();
+        log.warn("参数校验失败: {}", e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        commonResp.setSuccess(false);
+        commonResp.setMessage(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        return commonResp;
+    }
+}
