095496c331647f44ce2923de7f01d8f2049ea7e4
wxvirus <1418667580@qq.com>
2021-12-20 21:51:31 +0800

    6-9 增加雪花算法工具类，完成电子书新增功能

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -3,6 +3,11 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
+      <p>
+        <a-button type="primary" @click="add" size="large">
+          新增
+        </a-button>
+      </p>
       <a-table
           :columns="columns"
           :row-key="record => record.id"
@@ -168,6 +173,13 @@ export default defineComponent({
       ebook.value = record
     }
 
+    // 新增
+    const add = () => {
+      modalVisible.value = true;
+      // 清空
+      ebook.value = {}
+    }
+
     onMounted(() => {
       handleQuery({
         page: 1,
@@ -182,6 +194,7 @@ export default defineComponent({
       loading,
       handleTableChange,
       edit,
+      add,
       ebook,
       modalVisible,
       modalLoading,
