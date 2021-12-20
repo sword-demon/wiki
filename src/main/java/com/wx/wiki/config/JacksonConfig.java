d831d3e4f5c3186621d8f73cd455442eecd1d06c
wxvirus <1418667580@qq.com>
2021-12-20 23:51:13 +0800

    6-12 统一注解处理雪花算法的精度丢失 加上 @Bean 注解

--- src/main/java/com/wx/wiki/config/JacksonConfig.java
+++ src/main/java/com/wx/wiki/config/JacksonConfig.java
@@ -3,6 +3,7 @@ package com.wx.wiki.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
@@ -12,6 +13,7 @@ import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 @Configuration
 public class JacksonConfig {
 
+    @Bean
     public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
         ObjectMapper objectMapper = builder.createXmlMapper(false).build();
         SimpleModule simpleModule = new SimpleModule();
