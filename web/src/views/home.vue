0a9153a576a1f0440cf00a6583c1160ceb41cbfa
wxvirus <1418667580@qq.com>
2021-12-23 23:13:41 +0800

    7-7 首页显示分类菜单

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -5,41 +5,23 @@
           mode="inline"
           :style="{ height: '100%', borderRight: 0 }"
       >
-        <a-sub-menu key="sub1">
-          <template #title>
+        <a-menu-item key="welcome">
+          <route-link :to="'/'">
+            <MailOutlined />
+            <span>欢迎</span>
+          </route-link>
+        </a-menu-item>
+        <a-sub-menu v-for="item in level1" :key="item.id">
+          <template v-slot:title>
               <span>
                 <user-outlined/>
-                subnav 1
+                {{item.name}}
               </span>
           </template>
-          <a-menu-item key="1">option1</a-menu-item>
-          <a-menu-item key="2">option2</a-menu-item>
-          <a-menu-item key="3">option3</a-menu-item>
-          <a-menu-item key="4">option4</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-              <span>
-                <laptop-outlined/>
-                subnav 2
-              </span>
-          </template>
-          <a-menu-item key="5">option5</a-menu-item>
-          <a-menu-item key="6">option6</a-menu-item>
-          <a-menu-item key="7">option7</a-menu-item>
-          <a-menu-item key="8">option8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-          <template #title>
-              <span>
-                <notification-outlined/>
-                subnav 3
-              </span>
-          </template>
-          <a-menu-item key="9">option9</a-menu-item>
-          <a-menu-item key="10">option10</a-menu-item>
-          <a-menu-item key="11">option11</a-menu-item>
-          <a-menu-item key="12">option12</a-menu-item>
+          <a-menu-item v-for="child in item.children" :key="child.id">
+            <MailOutlined />
+            <span>{{child.name}}</span>
+          </a-menu-item>
         </a-sub-menu>
       </a-menu>
     </a-layout-sider>
@@ -73,6 +55,8 @@
 <script lang="ts">
 import { defineComponent, onMounted, ref } from 'vue';
 import axios from 'axios';
+import {message} from 'ant-design-vue'
+import {Tool} from '@/util/tool'
 
 export default defineComponent({
   name: 'home',
@@ -82,8 +66,32 @@ export default defineComponent({
     const ebooks = ref(); // 响应式的数据
     // const ebooks1 = reactive({books: []}); // 响应式的数据 方式2
 
+    const level1 = ref()
+    let categorys: any
+
+    // 查询所有分类
+    const handleQueryCategory = () => {
+      axios.get("/category/all").then((response) => {
+        const data = response.data
+        if (data.success) {
+          categorys = data.content
+          console.log('原始数组: ', categorys)
+
+          level1.value = []
+          level1.value = Tool.array2Tree(categorys, 0)
+          console.log('树形结构: ', level1.value)
+        } else {
+          message.error(data.message)
+        }
+      })
+    }
+
     // 生命周期函数里比较适合写初始化内容
     onMounted(() => {
+
+      // 初始加载分类
+      handleQueryCategory()
+
       axios.get("/ebook/list", {
         params: {
           page: 1,
@@ -107,7 +115,8 @@ export default defineComponent({
     return {
       ebooks,
       // books: toRef(ebooks1, "books"),
-      actions
+      actions,
+      level1
     }
   }
 });
