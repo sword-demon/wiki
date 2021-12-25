64cea573fdd3f2b596d5e3e1177eff55f3916add
wxvirus <1418667580@qq.com>
2021-12-25 17:36:43 +0800

    8-7 集成富文本框插件wangeditor@4.6.3 在文档表单中显示富文本框

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -73,6 +73,9 @@
       <a-form-item label="排序">
         <a-input v-model:value="doc.sort"/>
       </a-form-item>
+      <a-form-item label="内容">
+        <div id="content"></div>
+      </a-form-item>
     </a-form>
   </a-modal>
 </template>
@@ -84,6 +87,7 @@ import { message, Modal } from 'ant-design-vue'
 import { Tool } from '@/util/tool'
 import { useRoute } from "vue-router";
 import ExclamationCircleOutlined from "@ant-design/icons-vue/ExclamationCircleOutlined";
+import E from 'wangeditor'
 
 export default defineComponent({
   name: 'AdminDoc',
@@ -159,6 +163,8 @@ export default defineComponent({
     const doc = ref()
     const modalVisible = ref(false)
     const modalLoading = ref(false)
+    const editor = new E("#content")
+
     const handleModalOk = () => {
       modalLoading.value = true
       axios.post("/doc/save", doc.value).then((response) => {
@@ -248,6 +254,10 @@ export default defineComponent({
 
       // 为选择树添加一个 "无"字
       treeSelectData.value.unshift({id: 0, name: '无'})
+
+      setTimeout(() => {
+        editor.create()
+      }, 100)
     }
 
     // 新增
@@ -261,6 +271,10 @@ export default defineComponent({
       treeSelectData.value = Tool.copy(level1.value)
       // 为选择树添加一个 "无"字
       treeSelectData.value.unshift({id: 0, name: '无'})
+
+      setTimeout(() => {
+        editor.create()
+      }, 100)
     }
 
     // 删除
