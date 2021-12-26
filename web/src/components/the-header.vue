31c4ed1d7f8118a5e49ab98fd8dd2cb009156a2f
wxvirus <1418667580@qq.com>
2021-12-26 15:39:49 +0800

    9-8 前端增加登录框

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -1,6 +1,6 @@
 <template>
   <a-layout-header class="header">
-    <div class="logo" />
+    <div class="logo"/>
     <a-menu
         theme="dark"
         mode="horizontal"
@@ -21,14 +21,85 @@
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
+      <a class="login-menu" @click="showLoginModal">
+        <span>登录</span>
+      </a>
     </a-menu>
+
+    <a-modal
+        title="登录"
+        v-model:visible="loginModalVisible"
+        :confirm-loading="loginModalLoading"
+        @ok="login">
+      <a-form :model="loginUser" :label-col="{span: 6}" :wrapper-col="{span: 18}">
+        <a-form-item label="登录名">
+          <a-input v-model:value="loginUser.loginName"></a-input>
+        </a-form-item>
+        <a-form-item label="密码">
+          <a-input v-model:value="loginUser.password" type="password"></a-input>
+        </a-form-item>
+      </a-form>
+    </a-modal>
   </a-layout-header>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref, computed } from 'vue'
+import axios from "axios";
+import { message } from "ant-design-vue";
+// import store form '@/store'
+
+declare let hexMd5: any
+declare let KEY: any
 
 export default defineComponent({
-  name: 'the-header'
+  name: 'the-header',
+  setup() {
+
+    // 用来登录
+    const loginUser = ref({
+      loginName: 'test',
+      password: 'test'
+    })
+    const loginModalVisible = ref(false)
+    const loginModalLoading = ref(false)
+    // 显示登录模态框
+    const showLoginModal = () => {
+      loginModalVisible.value = true
+    }
+
+    // 登录
+    const login = () => {
+      console.log('开始登录')
+      loginModalLoading.value = true
+      loginUser.value.password = hexMd5(loginUser.value.password + KEY)
+      axios.post("/user/login", loginUser.value).then((res) => {
+        loginModalLoading.value = false
+        const data = res.data
+        if (data.success) {
+          loginModalVisible.value = false
+          message.success("登录成功!")
+        } else {
+          message.error(data.message)
+        }
+      })
+    }
+
+    return {
+      loginModalVisible,
+      loginModalLoading,
+      showLoginModal,
+      loginUser,
+      login
+    }
+  }
 })
 </script>
+
+<style>
+.login-menu {
+  float: right;
+  color: white;
+  padding-left: 10px;
+}
+</style>
