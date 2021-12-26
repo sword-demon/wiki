5f8b85753befb69a5e1bd6452a0ac03396e99e34
wxvirus <1418667580@qq.com>
2021-12-26 16:05:28 +0800

    9-9 集成redis，登录成功后将登录信息放入redis中

--- src/main/java/com/wx/wiki/controller/UserController.java
+++ src/main/java/com/wx/wiki/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.wx.wiki.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.wx.wiki.req.UserLoginReq;
 import com.wx.wiki.req.UserQueryReq;
 import com.wx.wiki.req.UserResetPassword;
@@ -9,18 +10,31 @@ import com.wx.wiki.resp.UserLoginResp;
 import com.wx.wiki.resp.UserQueryResp;
 import com.wx.wiki.resp.PageResp;
 import com.wx.wiki.service.UserService;
+import com.wx.wiki.util.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user") // 提取请求地址前缀
 public class UserController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+
     @Resource
     private UserService userService;
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    @Resource
+    private SnowFlake snowFlake;
 
     @GetMapping("/list")
     public CommonResp<PageResp<UserQueryResp>> list(@Valid UserQueryReq req) {
@@ -58,11 +72,18 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public CommonResp logn(@Valid @RequestBody UserLoginReq req) {
+    public CommonResp login(@Valid @RequestBody UserLoginReq req) {
         // 十六进制的md5加密
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp<UserLoginResp> resp = new CommonResp<>();
         UserLoginResp userLoginResp = userService.login(req);
+
+        // 生成单点登录token并放入redis中
+        Long token = snowFlake.nextId();
+        LOG.info("生成单点登录token: {}, 并放入redis中", token);
+        userLoginResp.setToken(token.toString());
+        // userLoginResp 要放入 redis 需要序列化一下
+        redisTemplate.opsForValue().set(token, JSONObject.toJSONString(userLoginResp), 3600 * 24, TimeUnit.SECONDS);
         resp.setContent(userLoginResp);
         return resp;
     }
