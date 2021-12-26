cae8fd9725691ee1305b8e2aa125b3b2ba4b9342
wxvirus <1418667580@qq.com>
2021-12-26 16:12:50 +0800

    9-9 增加了redis的测试接口

--- src/main/java/com/wx/wiki/controller/TestController.java
+++ src/main/java/com/wx/wiki/controller/TestController.java
@@ -2,13 +2,18 @@ package com.wx.wiki.controller;
 
 import com.wx.wiki.domain.Test;
 import com.wx.wiki.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 测试代码
@@ -16,8 +21,13 @@ import java.util.List;
 @RestController
 public class TestController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
     @Resource
     private TestService testService;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @Value("${test.hello:TEST}")
     private String testHello;
@@ -30,5 +40,19 @@ public class TestController {
     @GetMapping("/test/list")
     public List<Test> list() {
         return testService.list();
+    }
+
+    @RequestMapping("/redis/set/{key}/{value}")
+    public String set(@PathVariable Long key, @PathVariable String value) {
+        redisTemplate.opsForValue().set(key, value, 3600, TimeUnit.SECONDS);
+        LOG.info("key: {}, value: {}", key, value);
+        return "success";
+    }
+
+    @RequestMapping("/redis/get/{key}")
+    public Object get(@PathVariable Long key) {
+        Object object = redisTemplate.opsForValue().get(key);
+        LOG.info("key: {}, value: {}", key, object);
+        return object;
     }
 }
