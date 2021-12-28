05918c7ffd03863e7a8d4bc01029a63ee4853979
wxvirus <1418667580@qq.com>
2021-12-28 21:38:25 +0800

    10-7 增加日志流水号，方便生产运维

--- src/main/java/com/wx/wiki/job/DocJob.java
+++ src/main/java/com/wx/wiki/job/DocJob.java
@@ -1,8 +1,10 @@
 package com.wx.wiki.job;
 
 import com.wx.wiki.service.DocService;
+import com.wx.wiki.util.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -18,11 +20,16 @@ public class DocJob {
     @Resource
     private DocService docService;
 
+    @Resource
+    private SnowFlake snowFlake;
+
     /**
      * 每30秒更新电子书信息
      */
     @Scheduled(cron = "5/30 * * * * ? ")
     public void cron() {
+        // 增加日志流水号
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
 //        SimpleDateFormat format = new SimpleDateFormat("mm:ss");
 //        String dateString = format.format(new Date());
 //        Thread.sleep(1500);
