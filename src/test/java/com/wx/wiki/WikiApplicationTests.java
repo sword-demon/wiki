8416810da0b8bda5b3a574d0d4f1b81cde826d69
wxvirus <1418667580@qq.com>
2022-01-01 19:04:45 +0800

    12-6 打包java项目，设置sql，默认字段为0

--- src/test/java/com/wx/wiki/WikiApplicationTests.java
+++ src/test/java/com/wx/wiki/WikiApplicationTests.java
@@ -1,34 +1,34 @@
-package com.wx.wiki;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class WikiApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-    @Test
-    void showAppleToPeaceAndLove() {
-        System.out.println("                                \n" +
-                "                █    ▄          \n" +
-                "           ▀▄▄  █ ▄██▀          \n" +
-                "            ▀▀█▄██▀▀            \n" +
-                "       ▄▄▄▄▄▄▄▄██▄▄▄▄▄▄▄▄▄▄     \n" +
-                "    ▄█▀▀▀▀▀▀▀███████▀▀▀▀▀▀▀█▄▄  \n" +
-                "  ▄██▀                     ▀ █▄ \n" +
-                "  █▀                     ▄    █ \n" +
-                "  █                     ██     █\n" +
-                "  █                   ███▀     █\n" +
-                "  █                   ██▀      █\n" +
-                "  █▄                 █▀▀      ██\n" +
-                "  ██                 █       ██ \n" +
-                "   ██                       ▄█▀ \n" +
-                "   ▀█▄                    ▄▄█▀  \n" +
-                "    ▀███                  ██    \n" +
-                "      ▀██▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█▀     ");
-    }
-
-}
+//package com.wx.wiki;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class WikiApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//    @Test
+//    void showAppleToPeaceAndLove() {
+//        System.out.println("                                \n" +
+//                "                █    ▄          \n" +
+//                "           ▀▄▄  █ ▄██▀          \n" +
+//                "            ▀▀█▄██▀▀            \n" +
+//                "       ▄▄▄▄▄▄▄▄██▄▄▄▄▄▄▄▄▄▄     \n" +
+//                "    ▄█▀▀▀▀▀▀▀███████▀▀▀▀▀▀▀█▄▄  \n" +
+//                "  ▄██▀                     ▀ █▄ \n" +
+//                "  █▀                     ▄    █ \n" +
+//                "  █                     ██     █\n" +
+//                "  █                   ███▀     █\n" +
+//                "  █                   ██▀      █\n" +
+//                "  █▄                 █▀▀      ██\n" +
+//                "  ██                 █       ██ \n" +
+//                "   ██                       ▄█▀ \n" +
+//                "   ▀█▄                    ▄▄█▀  \n" +
+//                "    ▀███                  ██    \n" +
+//                "      ▀██▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█▀     ");
+//    }
+//
+//}
